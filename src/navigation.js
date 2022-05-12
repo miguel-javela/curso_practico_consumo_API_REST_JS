513c45369dff95181842baa2d1e064a05d36807d
miguel javela <mjavela@compuamerica.com.ve>
2022-05-11 21:24:37 -0500

    clase 11 - filtrando peliculas por categoria

--- src/navigation.js
+++ src/navigation.js
@@ -64,6 +64,12 @@ function categoriesPage (){
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.remove('inactive');
     movieDetailSection.classList.remove('inactive');
+
+    const [_,categoryData] = location.hash.split('=');
+    const [categoryId,categoryName] = categoryData.split('-');
+    headerCategoryTitle.innerHTML = categoryName;
+
+    getMoviesByCategory(categoryId);
 }
 
 
